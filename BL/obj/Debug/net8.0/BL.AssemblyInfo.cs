b34e4fd5361c7157a0199b23e980e224dca0c85f
שיף חני < @GurSeminar.Domain>
2025-04-23 17:55:23 +0300

    upload C#

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7881967738286c28aecf3690794d014611243f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de9fbb17b1d18360c2cc5035e34ffe115909fd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
